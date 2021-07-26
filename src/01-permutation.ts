598ae093e34ef495e33abd92ff97e9a91a5ce9f5
lixiangyu <654762937@qq.com>
2021-07-26 23:08:10 +0800

    permutation

---
+++ src/01-permutation.ts
@@ -0,0 +1,29 @@
+function remove<T>(set: Set<T>, i: T) {
+  const newSet = new Set<T>([...set])
+  newSet.delete(i)
+  return newSet
+}
+
+function permutation(str: string) {
+  function R(set: Set<string>): Array<string> {
+    if (set.size === 1) {
+      return [set.values().next().value]
+    }
+
+    return flattern(
+      [...set].map((c) => R(remove(set, c)).map((perm) => c + perm))
+    )
+  }
+
+  return R(new Set([...str]))
+}
+
+function flattern(arr: any[]): any[] {
+  console.log('arr', arr)
+  if (!Array.isArray(arr)) {
+    return arr
+  }
+  return ([] as any[]).concat(...arr.map(flattern))
+}
+
+console.log(permutation('abc'))
